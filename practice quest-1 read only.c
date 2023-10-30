acb49c00ac918edcc2522ec5279a79b1381463f2
Fiza Farzand <fizafarzand@gmail.com>
2023-10-31 02:48:24 +0500

    exercise questions and practice questions

---
+++ practice quest-1 read only.c
@@ -0,0 +1,114 @@
+#include<stdio.h>
+/*
+These are practice codes for better understanding
+*/
+
+//CODE NO 1
+
+//int main() {
+//    int age;
+//    printf("enter your age:");
+//    scanf("%d",&age);
+//    printf("your age is %d",age);
+//    return 0;
+//}
+
+
+//CODE NO 2
+
+
+//int main() {
+//    int a,b;
+//    printf("enter value of a:");
+//    scanf("%d",&a);
+//    printf("enter value of b:");
+//    scanf("%d",&b);
+//    int sum = a+b;
+//    printf("sum is:%d",sum);
+//   return 0;
+//}
+
+//CODE NO 3
+
+
+//AREA OF A SQUARE
+
+//int main() {
+//    int side;
+//    printf("enter side:");
+//    scanf("%d",&side);
+//
+//    printf("area is: %d",side*side);
+//
+//    return 0;
+//}
+
+
+
+//CODE NO 4
+
+
+
+//int main() {
+//    int a = (int)1.99999;
+//    printf("%d",a);
+//return 0;
+//}
+
+
+//CODE NO 5
+
+
+//int main() {
+//   int x;
+//   printf("enter a number");
+//   scanf("%d",&x);
+//   printf("%d",x%2==0);
+//   return 0;
+//}
+
+
+//CODE NO 6
+
+//int main() {
+//int age;
+//printf("Enter age : ");
+//scanf("%d", &age);
+//if(age < 12) {
+//printf("child");
+//}
+//else if(age < 18) {
+//printf("teenager");
+//}
+//else {
+//printf("adult");
+//}
+//return 0;
+//}
+
+
+//CODE N0 7
+
+//#include<math.h>
+//int main() {
+//int day = 5;
+//switch(day) {
+//case 1 : printf("monday \n");
+//break;
+//case 2 : printf("tuesday \n");
+//break;
+//case 3 : printf("wednesday \n");
+//break;
+//case 4 : printf("thursday \n");
+//break;
+//case 5 : printf("friday \n");
+//break;
+//case 6 : printf("saturday \n");
+//break;
+//case 7 : printf("sunday \n");
+//break;
+//
+//}
+//return 0;
+//}
+
---
+++ practice quests-2.c
@@ -0,0 +1,147 @@
+//include<stdio.h>
+
+/*
+CODE NO 1
+*/
+
+//int main() {
+//   int isSunday=1;
+//   int isSnowing =1;
+//   printf("%d \n",isSunday && isSnowing);
+//   return 0;
+//}
+
+/*
+CODE NO 2
+*/
+
+//int main() {
+//   int isMonday=1;
+//   int isRaining =0;
+//   printf("%d \n",isMonday || isRaining);
+//   return 0;
+//}
+
+
+/*
+CODE NO 3
+*/
+
+//#include<math.h>
+//int main() {
+//   int x;
+//   printf("enter a number:");
+//   scanf("%d",&x);
+//   prinf("%d \n",x>9 && x<100);
+//   return 0;
+//}
+
+/*
+CODE NO 4
+*/
+
+
+//int main() {
+//    double num1, num2, num3, average;
+//
+//    printf("Enter the first number: ");
+//    scanf("%lf", &num1);
+//
+//    printf("Enter the second number: ");
+//    scanf("%lf", &num2);
+//
+//    printf("Enter the third number: ");
+//    scanf("%lf", &num3);
+//
+//    // Calculate the average
+//    average = (num1 + num2 + num3) / 3.0;
+//
+//    // Print the average
+//    printf("The average of %.2lf, %.2lf, and %.2lf is: %.2lf\n", num1, num2, num3, average);
+//
+//    return 0;
+//}
+
+/*
+CODE NO 5
+*/
+
+
+//int main() {
+//    char character;
+//    printf("Enter a character:");
+//    scanf("%c",&character);
+//    if (character >= '0' && character<= '9') {
+//        printf("%c is a digit");}
+//    else {
+//        printf("%c is not a digit");
+//
+//    }
+//    return 0;
+//    }
+
+
+
+//int main() {
+//    int n, num, smallest;
+//    printf("Enter the number of elements: ");
+//    scanf("%d", &n);
+//    if (n <= 0) {
+//        printf("Please enter a valid number of elements.\n");
+//        return 1;
+//    }
+//
+//    printf("Enter element 1: ");
+//    scanf("%d", &smallest);
+//    for (int i = 2; i <= n; i++) {
+//        printf("Enter element %d: ", i);
+//        scanf("%d", &num);
+//
+//        if (num < smallest) {
+//            smallest = num;
+//        }
+//    }
+//    printf("The smallest number is: %d\n", smallest);
+//
+//   return 0;
+//}
+
+
+//CODE NO 7
+
+
+//int main() {
+//    int age;
+//    printf("enter your age:");
+//    scanf("%d ",&age);
+//    age>=18? printf("adult"): printf("not adult");
+//    return 0;
+//}
+
+
+
+//CODE NO 8
+
+
+
+//int main() {
+//    int number;
+//    printf("enter a number:");
+//    scanf("%d",&number);
+//    if(number>=0) {
+//        printf("positive num \n ");
+//        if(number%2==0) {
+//            printf("its even");
+//        }
+//        else {
+//            printf(" its oddd");
+//        }
+//    }
+//    else {
+//        printf("negative num");
+//    }
+//    return 0;
+//    }
+
+
+
